e84efc9bb7e196ac2ba8a6fc341b06362099893e
Kris Brown <kris@topos.institute>
2023-02-26 11:41:39 -0800

    new structure

--- scripts/obsidian.jl
+++ scripts/obsidian.jl
@@ -32,16 +32,18 @@ end
 function get_links()
   links_out = Dict{String,Vector{PSS}}()
   title_dict = Dict{String,String}()
+  path_dict = Dict{String,String}()
   for (root, _, files) in walkdir(VAULT) 
     for fi in filter(is_md, files)
       P = fi[1 : end - 3]
       p = fix_title(P)
       title_dict[p]=P
+      path_dict[p] = replace(root, VAULT=>"")
       links_out[p] = get_links(joinpath(VAULT,root,fi))
     end 
   end 
   links_in = get_links_in(links_out)
-  return title_dict, links_in, links_out
+  return title_dict, links_in, links_out, path_dict
 end 
 
 rm_link(txt) = replace(txt, r"\[(.*?)\]\(.*?\)"=> s"\g<1>")
@@ -76,47 +78,56 @@ function get_links_in(links_out)
 end 
 
 """Render an [[internal|link]] as a [markdown](link) in a .qmd file"""
-function replace_link(title_dict)
+function replace_link(title_dict, path_dict)
   function f(mtch)
     res = split(mtch[3:end-2],"|")
     ttl, disp = length(res) == 1 ? repeat(res,2) : res
     ttlp = split(ttl,"#")
     ttl, p = length(ttlp) == 1 ? (ttl,"") : (ttlp[1],"#"*ttlp[2])
-    return "[$disp](/$(joinpath(OBSID,title_dict[fix_title(ttl)])).html$p)"
+    fixed_title = fix_title(ttl)
+    return "[$disp](/$(joinpath(OBSID,path_dict[fixed_title], title_dict[fixed_title])).html$p)"
   end 
 end
 
-function process(pthfi, links_in, title_dict)
+function process(pthfi, links_in, title_dict, path_dict)
   fi = last(split(pthfi,"/"))
+  path_fi = joinpath(path_dict[fi], fi)
   real_name = joinpath(dirname(pthfi), title_dict[fi]) * ".md"
-  println("processing $fi ($real_name)\n\n")
+  println("processing $path_fi ($real_name)\n\n")
   txt = read(real_name, String)
   if !isempty(links_in)
     # Add the backlinks file to backlinks folder
-    render_links(fi, links_in)
+    render_links(fi, links_in, path_dict)
     # Add the path to backlinks file in the metadata 
     t1,t2,t3... = split(txt,"---\n")
-    n = count(==('/'), joinpath(OBSID,fi))
+    n = count(==('/'), joinpath(OBSID,path_fi))
     txt = join([t1,t2*show_listing(n,fi),t3...],"---\n")
   end 
-  txt = replace(txt,r"!\[\[([^\]]+)\]\]" => s"{{< include \"\1.qmd\" >}}")
-  txt = replace(txt,r"\[\[([^\]]+)\]\]" => replace_link(title_dict))
+  txt = replace(txt,r"!\[\[([^\]]+)\]\]" => include_link(path_dict))
+  txt = replace(txt,r"\[\[([^\]]+)\]\]" => replace_link(title_dict, path_dict))
   txt = replace(txt, "```mermaid"=>"```{mermaid}")
-  newfi = joinpath(QUART, OBSID, fi*".qmd")
+  newfi = joinpath(QUART, OBSID, path_dict[fi], fi*".qmd")
   mkpath(dirname(newfi))
   open(newfi, "w") do io
     write(io, txt)
   end
 end
+function include_link(path_dict)
+  function f(mtch)
+    fixed_title = fix_title(mtch[4:end-2])
+    println("path_dict[fixed_title] $(path_dict[fixed_title])")
+    return "{{< include \"/$(joinpath(OBSID,path_dict[fixed_title], fixed_title)).qmd\" >}}"
+  end
+end 
 
 """create a backlinks yaml file"""
-function render_links(fi, links_in)
+function render_links(fi, links_in, path_dict)
   kpth = joinpath(BACK,"$fi.yml")
   mkpath(dirname(kpth))
   open(kpth, "w") do io
     for (link, ctx) in links_in 
       println(io,"- name: $(last(split(link,"/")))")
-      println(io,"  href: /$(joinpath(OBSID,link)).html")
+      println(io,"  href: /$(joinpath(OBSID,path_dict[link],link)).html")
       println(io,"  custom-field: \"$ctx\"")
     end
   end
@@ -136,11 +147,11 @@ show_listing(ndots::Int,name::AbstractString)  =
 ########
 # MAIN # 
 ########
-title_dict, links_in, links_out = get_links()
+title_dict, links_in, links_out, path_dict = get_links()
 for (root, _, files) in walkdir(VAULT) 
   for file in filter(is_md, files)
     fi = fix_title(file[1:end-3])
     l_in = links_in[fi]
-    process(joinpath(root,fi), l_in, title_dict)
+    process(joinpath(root,fi), l_in, title_dict, path_dict)
   end 
 end 
